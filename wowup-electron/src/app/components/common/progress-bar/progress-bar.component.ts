4a951b53feea9f537feacdedc5c9c2bb21fc6bc6
jliddev <jliddev@gmail.com>
2023-11-30 16:44:11 -0600

    Adjust the progress button
    Create custom progress bar for reasons

---
+++ wowup-electron/src/app/components/common/progress-bar/progress-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-progress-bar",
+  templateUrl: "./progress-bar.component.html",
+  styleUrls: ["./progress-bar.component.scss"],
+})
+export class ProgressBarComponent {
+  @Input() value: number;
+  
+}
